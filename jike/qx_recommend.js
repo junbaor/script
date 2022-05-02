3361ee83039a6d05912571d33c21da3ef77fec12
junbaor <junbaor@gmail.com>
2022-05-03 03:17:47 +0800

    Create qx_recommend.js

---
+++ jike/qx_recommend.js
@@ -0,0 +1,19 @@
+const url = "https://jike.junbao.me/api/recommendFeed";
+const method = "POST";
+const headers = { "Content-Type": "application/json" };
+const data = {};
+
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+    body: JSON.stringify(data)
+};
+
+$task.fetch(myRequest).then(response => {
+    $done(response.body);
+}, reason => {
+    console.log("替换即刻推荐动态失败, 原因: " + reason.error);
+    $notify("😿 替换即刻推荐动态失败", "", "原因: " + reason.error + " 请刷新重试");
+    $done();
+});
