9f545baeea7dcef6ae9a1c4decd45f6dccd6210d
junbaor <junbaor@gmail.com>
2022-06-07 01:32:13 +0800

    Create qx_recommend_v3.js

---
+++ jike/qx_recommend_v3.js
@@ -0,0 +1,19 @@
+const url = "https://jike.asynctask.cn/api/recommendFeed";
+const method = "POST";
+const headers = { "Content-Type": "application/json" };
+const data = {};
+
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+    body: JSON.stringify(data)
+};
+
+$task.fetch(myRequest).then(response => {
+    $done(response.body);
+}, reason => {
+    console.log("😿 替换动态广场失败, 原因: " + reason.error);
+    $notify("😿 替换动态广场失败", "", "原因: " + reason.error + " 请刷新重试");
+    $done();
+});
